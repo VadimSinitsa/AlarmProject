9089e04fc8b06e6226a41c42da990f4153fa2c51
VadimSinitsa <141561499+VadimSinitsa@users.noreply.github.com>
2023-08-15 00:03:18 +0300

    Project created

---
+++ AlarmProject/AlarmProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlarmProject
+//
+//  Created by Vadim Sinitsa on 14.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
